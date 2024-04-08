4c914bc9e067a08758845f716fee34c72ab3fca8
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-04-08 05:30:24 +0000

    network memory

--- os module/index.js
+++ os module/index.js
@@ -5,3 +5,5 @@ console.log("cpu Architecture"+ os.arch());
 console.log("Free memory:" + os.freemem() );
 
 console.log("Total memory:" +os.totalmem());
+
+console.log("Netwoork memory:" + os.networkInterfaces());
